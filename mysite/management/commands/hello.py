5839ddb84c5d6d752f2d502a30410c7294b23f29
Andy Burton <aburton@eab.com>
2015-09-02 08:12:45 -0500

    Testing using models in a management/command

--- mysite/management/commands/hello.py
+++ mysite/management/commands/hello.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand
+from mysite.models import People
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         print "Hello, World!"
+        print People.objects.count()
